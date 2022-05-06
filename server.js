8ae22781d34419f2b26eba9a52b3c2b5f88dbdb4
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-05-06 16:42:06 +0800

    連接資料庫、實作註冊、取得會員資料、image++

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+require('dotenv').config()
 const bcrypt = require("bcrypt");
 const cors = require('cors')
 const express = require("express");
+const knex = require('knex');
+const db = knex({
+  client:'pg',
+  connection:{
+    host:'127.0.0.1',
+    user:'postgres',
+    password:process.env.DB_PWD,
+    database:'smartbrainDB'
+  }  
+})
+
+db.select('*').from('users').then(data=>{
+  console.log(data)
+})
 const app = express();
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
@@ -19,18 +34,18 @@ app.listen(3500, () => {
 
 //二、模擬一下Database假資料
 let dbUsers=0
-const db = {
-  users: [
-    // {
-    //   id: "123",
-    //   name: "John",
-    //   email: "fgffg@hotmail.com",
-    //   password: "dfdfdf",
-    //   entries: 0,
-    //   joined: new Date(),
-    // },
-  ],
-};
+// const db = {
+//   users: [
+//     // {
+//     //   id: "123",
+//     //   name: "John",
+//     //   email: "fgffg@hotmail.com",
+//     //   password: "dfdfdf",
+//     //   entries: 0,
+//     //   joined: new Date(),
+//     // },
+//   ],
+// };
 
 app.get("/", (req, res) => {
   console.log('有人要求/',db.users)
@@ -66,48 +81,78 @@ app.post("/sigin", (req, res) => {
 });
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  let hashPassword = "";
-  dbUsers++
-  const userData = {
-    id: dbUsers,
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  };
-  bcrypt.hash(password, 10, function (err, hash) {
-    hashPassword = hash;
-    userData.password = hashPassword
-    db.users.push(userData);
-    console.log(userData)
-    res.json(userData);
-  });
+  // let hashPassword = "";
+  // dbUsers++
+  // const userData = {
+  //   id: dbUsers,
+  //   name: name,
+  //   email: email,
+  //   entries: 0,
+  //   joined: new Date(),
+  // };
+  db('users')
+  .returning('*')  //會回傳插入的所有資料
+  .insert({
+    name:name,
+    email:email,
+    joined:new Date()
+  })
+  .then(user=>{
+    console.log(user)
+    res.json(user[0])
+  })
+  .catch(err=>{
+    res.status(400).json(err)
+  })
+  // bcrypt.hash(password, 10, function (err, hash) {
+  //   hashPassword = hash;
+  //   userData.password = hashPassword
+  //   db.users.push(userData);
+  //   console.log(userData)
+  //   res.json(userData);
+  // });
 });
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
-  let found = false;
-  db.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  // let found = false;
+  db.select('*').from('users').where({id})
+  .then(user=>{ //找不到會回傳空陣列
+    if(user.length){
+      res.json(user[0])
+    }else{
+      res.status(400).json('Not found')
     }
-  });
-  if (!found) {
-    res.status(400).json("not found");
-  }
+  })
+  .catch(err=>res.status(400).json(err))
+  // db.users.forEach((user) => {
+  //   if (user.id === id) {
+  //     found = true;
+  //     return res.json(user);
+  //   }
+  // });
+  // if (!found) {
+  //   res.status(400).json("not found");
+  // }
 });
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  console.log(id)
-  let found = false;
-  db.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("not found");
-  }
+  db('users').where({id})
+  .increment('entries',1)
+  .returning('entries')
+  .then(entries=>{
+    res.json(entries[0]);
+  })
+  .catch(err=>res.status(400).json('err'))
+  // console.log(id)
+  // let found = false;
+  // db.users.forEach((user) => {
+  //   if (user.id === id) {
+  //     found = true;
+  //     user.entries++;
+  //     return res.json(user.entries);
+  //   }
+  // });
+  // if (!found) {
+  //   res.status(400).json("not found");
+  // }
 });
