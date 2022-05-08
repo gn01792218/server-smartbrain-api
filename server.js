1691483345950e95cfcea3ee557d08e7f22a581f
MSI\gn01792218 <gn01792218@hotmail.com>
2022-05-08 12:39:36 +0800

    整理不必要的代碼

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const bcrypt = require("bcrypt");
 const cors = require('cors')
 const express = require("express");
 const knex = require('knex');
+
+const app = express();
+const handleRegister = require('./controllers/register').handleRegister
+const handleSigin = require('./controllers/sigin').handleSigin
+const handleProfile = require('./controllers/profile').handleProfile
+const handleImage = require('./controllers/image').handleImage
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.listen(3500, () => {
+  console.log("server is listing 3500");
+});
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -12,17 +26,6 @@ const db = knex({
     database: 'smartbrainDB'
   }
 })
-const handleRegister = require('./controllers/register').handleRegister
-const handleSigin = require('./controllers/sigin').handleSigin
-const handleProfile = require('./controllers/profile').handleProfile
-const handleImage = require('./controllers/image').handleImage
-const app = express();
-app.use(cors())
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.listen(3500, () => {
-  console.log("server is listing 3500");
-});
 /*
     一、規劃需要的API
     1. 首頁==>res==>Home page
@@ -31,22 +34,7 @@ app.listen(3500, () => {
     4./profile/:userId==>GET==>userObject
     5./image==>PUT==>userObject
 */
-
-app.get("/", (req, res) => {
-  // res.json(db.users);
-});
 app.post("/sigin",handleSigin(db,bcrypt))
 app.post("/register",handleRegister(db,bcrypt))
 app.get("/profile/:id",handleProfile(db))
 app.put("/image",handleImage(db))
-// app.put("/image", (req, res) => {
-//   console.log(req.body)
-//   const { id } = req.body;
-//   db('users').where({ id })
-//     .increment('entries', 1)
-//     .returning('entries')
-//     .then(entries => {
-//       res.json(entries[0]);
-//     })
-//     .catch(err => res.status(400).json('err'))
-// });
