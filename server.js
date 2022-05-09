3575d9756ff901728e6da9abcc7e91268d0ecd05
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-05-09 10:17:09 +0800

    連接heroku上的資料庫

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ app.listen(3500, () => {
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: process.env.DB_PWD,
-    database: 'smartbrainDB'
+    connectionString:process.env.DATABASE_URL,
+    ssl:true,
   }
 })
 /*
